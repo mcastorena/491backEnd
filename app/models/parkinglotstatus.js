d0f3ab3889a7a936ed1e5cb8143ab62ddbb3f584
Miguel <miguel.angel.castorena@gmail.com>
2019-03-07 17:09:32 -0800

    Update parkinglotstatus.js

--- app/models/parkinglotstatus.js
+++ app/models/parkinglotstatus.js
@@ -1,1 +1,20 @@
 
+const mongoose = require('mongoose');
+
+// Schema defines how status messages will be stored in MongoDB
+
+const ParkingLotStatusSchema = new mongoose.Schema({
+  parkinglot_ID: {
+    type: String,
+    required: true
+  },
+  status: {
+    type: String,
+    required: true
+  }
+},
+{
+  timestamps: true		// Saves createdAt and updatedAt
+});
+
+module.exports = mongoose.model('ParkingLotStatus', ParkingLotStatusSchema);
